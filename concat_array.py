86a695f23f3b34c28c34d37bd98a1be7e93fd3fa
tingonchan <tingonchan@gmail.com>
2016-06-07 13:24:31 -0600

    Create concat_array.py

---
+++ concat_array.py
@@ -0,0 +1,15 @@
+# declare an empty array
+a = np.array([])
+
+x = 1
+y = 2
+z = 3
+
+b = np.array([x, y, z])
+
+#first, a horz cat with b
+a = np.hstack((a, b))
+
+for i in range(0,10)
+    a = np.vstack((a, b))  # vertical concat
+    
